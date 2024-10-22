f07be5ef9e609b1a503782d3859eda5fdce27253
stacy <rafidisonstacy@gmail.com>
2024-10-22 19:24:50 +0300

    destination

--- views/destination.php
+++ views/destination.php
@@ -39,5 +39,19 @@
 
         </nav>
     </header>
+    <div class=un><a href=""><img src="34436609610_bae0a770d5_k-e1496139102149.jpg" alt=""></a></div>
+    <div class=deux><a href=""><img src="ampefy.jfif" alt=""></a></div>
+    <div class=trois><a href=""><img src="Baobab-de-majunga.jpg" alt=""></a></div>
+    <div class=quatre><a href=""><img src="bungalows-hotel-le-lagon-foulpointe-tamatave.jpg" alt=""></a></div>
+    <div class=cinq><a href=""><img src="island-slide02.jpg" alt=""></a></div>
+    <div class=six><a href=""><img src="Lagon-dIfaty-limite-par-un-recif-corallien.jpg" alt=""></a></div>
+    <div class=sept><a href=""><img src="logoMada.jpg" alt=""></a></div>
+    <div class=huit><a href=""><img src="mada-05.jpg" alt=""></a></div>
+    <div class=neuf><a href=""><img src="Madagascar-Antananarivo-Adobe-2-e1646900799426.jpg" alt=""></a></div>
+    <div class=dix><a href=""><img src="Madagascar-Antsirabe.png" alt=""></a></div>
+    <div class=onze><a href=""><img src="quelle-meilleure-periode-aller-nosy-be-2.jpg" alt=""></a></div>
+    <div class=douze><a href=""><img src="ville-de-toamasina.jpg" alt=""></a></div>
+    <div class=treize><a href=""><img src="" alt="madagascar-anton-balahz.jpg"></a></div>
+  
 </body>
 </html>
