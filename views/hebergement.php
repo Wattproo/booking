805bdb50c38a98a234132f1500dae3406e74f899
kewinezo <zoharimihajakewine@gmail.com>
2024-10-22 11:59:13 +0300

    modif_heberge

--- views/hebergement.php
+++ views/hebergement.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="publics/bootstrap-4.6.2-dist/css/bootstrap.css">
 </head>
-<body>
+<body class="hebergement">
     <header>
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
@@ -39,5 +39,63 @@
 
         </nav>
     </header>
+    <section>
+        <!-- Container for the image gallery -->
+        <div class="container">
+
+            <!-- Full-width images with number text -->
+            <div class="mySlides">
+            <div class="numbertext">1 / 5</div>
+                <img src="vuepiscine.jpg" style="width:100%">
+            </div>
+
+            <div class="Slides">
+            <div class="numbertext">2 / 5</div>
+                <img src="chambre1.web"  style="width:100%">
+            </div>
+
+            <div class="mySlides">
+            <div class="numbertext">3 / 5</div>
+                <img src="piscineExora.jpeg" style="width:100%">
+            </div>
+
+            <div class="mySlides">
+            <div class="numbertext">4 / 5</div>
+                <img src="tableExora.jpg" style="width:100%">
+            </div>
+
+            <div class="mySlides">
+            <div class="numbertext">5 / 5</div>
+                <img src="plage_entrevu.webp" style="width:100%">
+            </div>
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+            <!-- Image text -->
+            <div class="caption-container">
+            <p id="caption"></p>
+            </div>
+
+            <!-- Thumbnail images -->
+            <div class="row">
+            <div class="column">
+            <img class="demo cursor" src="vuepiscine.jpg" style="width:100%" onclick="currentSlide(1)" alt="la vue">
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="chambre1.webp" style="width:100%" onclick="currentSlide(2)" alt="piscine">
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="piscineExora.jpeg" style="width:100%" onclick="currentSlide(3)" alt="dressage">
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="tableExora.jpg" style="width:100%" onclick="currentSlide(4)" alt="elcaboussement">
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="plage_entrevu.webp" style="width:100%" onclick="currentSlide(5)" alt="chambre">
+            </div>
+        </div>
+    </section>
+    <script src="heberge.js"></script>
 </body>
 </html>
