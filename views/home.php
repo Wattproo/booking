24611f67a24d1acb3dc956591ad420f2d8f4aacd
MAMBA <jonathanmamba03@gmail.com>
2024-10-23 08:41:24 +0300

    Nouvelle NavBar

--- views/home.php
+++ views/home.php
@@ -38,36 +38,57 @@
                 <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
         </nav> -->
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-
-            <!-- Links -->
-            <ul class="navbar-nav">
-                <a class="navbar-brand" href="#">
-                    <img src="logoMada.jpg" alt="Logo" style="width: 70px; border-radius: 50%;">
-                </a>
-                <li class="nav-item">
-                  <a class="nav-link active" href="index.php?action=accueil">Accueil</a>
+    <header>
+        <p class="menuBar" onclick="afficheMenu()">
+            <i class="fa fa-bars"></i>
+        </p>
+        <nav class="navBar" bg="#016ac6">
+            <div class="navLogo">
+                <img src="logoMada.jpg" alt="Logo">
+            </div>
+            <ul class="navLinks">
+                <li>
+                    <a class="nav-link" href="index.php?action=accueil">accueil</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="index.php?action=destination">Destination</a>
+                <li>
+                    <a class="nav-link" href="index.php?action=destination">Déstination</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="index.php?action=hebergement">Hébergement</a>
+                <li>
+                    <a class="nav-link" href="index.php?action=hebergement">Hebergement</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?action=contact">Contact</a>
+                <li>
+                    <a class="nav-link active" href="index.php?action=contact">contact</a>
                 </li>
             </ul>
-            <form class="form-inline" action="/action_page.php">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-warning" type="submit">Search</button>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="index.php?action=logout"><span class="glyphicon glyphicon-log-in"></span>Se deconnecter</a></li>
-            </ul>
-
+            <div class="desconnect">
+                <i class="fa fa-user"></i>
+                <a href="#">Se Déconnecter</a>
+            </div>
+            <div class="form-inline">
+                <form action="/action_page.php">
+                    <input type="search" placeholder="Search" id="recherche">
+                    <label for="recherche"><i class="fa fa-search"></i></label>
+                </form>
+            </div>
         </nav>
     </header>
-
+    <script>
+        let navBar = document.querySelector(".navBar");
+        let menuBar = document.querySelector(".menuBar");
+        let form = document.querySelector(".form-inline")
+        let desconnect = document.querySelector(".desconnect");
+        function afficheMenu () {
+            navBar.classList.toggle("afficheMenu");
+            // form.style.position = "absolute";
+            // form.style.width = "25%";
+            // form.style.right = "2px";
+            desconnect.style.width = "15%";
+            desconnect.style.padding = "0px";
+            desconnect.style.textAlign = "center";
+        }
+        function survol() {
+            desconnect.classList.toggle("survol");
+        }
+    </script>
 </body>
 </html>
---
+++ views/script.js
@@ -0,0 +1,8 @@
+// let navBar = document.querySelector(".navBar");
+// let menuBar = document.querySelector(".menuBar");
+// let form = document.querySelector(".form-inline");
+
+
+// function afficheMenu () {
+//     navBar.classList.toggle("afficheMenu");
+// }
