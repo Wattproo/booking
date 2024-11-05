826c1650d1ae9700436ecf0de873cbd338ea34f0
Liana13M <lianaranarimanana@gmail.com>
2024-11-05 07:59:48 +0300

    heberge

---
+++ views/ampefy.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/ampefy.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+<div class="nosybe">
+  <div class="main" id="main1">
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Vous souhaitez vous détendre une nuit ou plus lors de vos transits entre deux vols intérieurs ou au retour d'une destination de province en vol intérieur la veille de votre départ, ou tout simplement découvrir la capitale Antananarivo et ses environs.
+  </p>
+
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/CJ table.jpg" alt="Mountains" style="width:100%">
+        <h3>Le petit-déjeuner</h3>
+        <p>Des plats gourmants à découvrir <a href="photo/CJ table.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/CJ vue.jpg" alt="Lights" style="width:100%">
+        <h3>La vue</h3>
+        <p>Vous êtes la bienvenue<a href="photo/CJ vue.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/CJ chambre.jpg" alt="Nature" style="width:100%">
+        <h3>Chambre de luxe</h3>
+        <p>Ainsi que des salles de bains <a href="photo/CJ chambre.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/CJ piscine.jpg" alt="Mountains" style="width:100%">
+        <h3>La piscine</h3>
+        <p>Idéale pour se rafraîchir <a href="photo/CJ piscine.jpg">voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/logo CJ.jpg" alt="Bear" style="width:100%">
+    <h3>CHEZ JEANNE  <a href="photo/logo CJ.jpg">voir Logo</a></h3>
+    <form action="index.php?action=ravintsaraoffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+  <br>
+  <br>
+
+  <!-- END MAIN -->
+  </div>
+  <br>
+  <br>
+  <div class="main" id="main1">
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Offrant une vue sur la piscine, le COCODY ROC propose un hébergement avec terrasse. L'établissement possède un restaurant et un parking privé gratuit. Il propose des chambres familiales et une piscine extérieure ouverte toute l'année.
+  </p>
+
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/CR vue.jpg" alt="Mountains" style="width:100%">
+        <h3>Une belle vue</h3>
+        <p>Propre envirennement autour <a href="photo/CR vue.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/CR resto.jpg" alt="Lights" style="width:100%">
+        <h3>Resto-bar</h3>
+        <p>Des boissons et snacks de goûts spécials<a href="photo/CR resto.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/CR chambre.webp" alt="Nature" style="width:100%">
+        <h3>Chambre de luxe</h3>
+        <p>Des chambres haut de gamme <a href="photo/CR chambre.webp">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/CR piscine.jpg" alt="Mountains" style="width:100%">
+        <h3>La piscine</h3>
+        <p>Idéale pour passer des moments innoubliables <a href="photo/CR piscine.jpg">voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/logo CR.jpg" alt="Bear" style="width:100%">
+    <h3>COCODY ROC  <a href="photo/logo CR.jpg">voir Logo</a></h3>
+    <form action="index.php?action=exoraoffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+
+  <!-- END MAIN -->
+  </div>
+  <br>
+  <br>
+  <div class="main">
+    <br>
+    <br>
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Doté d'une piscine avec vue, d'une baignoire en plein air et d'un jardin, l'Ecole Lodge Antananarivo propose un hébergement avec connexion Wi-Fi gratuite et vue sur le jardin. Bénéficiant d'une sécurité toute la journée, cet établissement possède également une cheminée extérieure. L'établissement possède un restaurant servant une cuisine africaine et un parking privé gratuit.
+
+  </p>
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/EL vue.jpg" alt="Mountains" style="width:100%">
+        <h3>La belle vue</h3>
+        <p>Des divers natures <a href="photo/EL vue.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/EL piscine.jpg" alt="Lights" style="width:100%">
+        <h3>Relaxation</h3>
+        <p>Une petite réunion de famille <a href="photo/EL piscine.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/EL chambre.jpg" alt="Nature" style="width:100%">
+        <h3>Vos chambres</h3>
+        <p>La simplicité  <a href="photo/EL chambre.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/EL table.jpg" alt="Mountains" style="width:100%">
+        <h3>Le petit-déjeuner</h3>
+        <p>Des cocktails à servir <a href="photo/EL table.jpg">Voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/logo EL.jpg" alt="Bear" style="width:100%">
+    <h3>ECOLE LOGDE <a href="photo/logo EL.jpg">voir logo</a></h3>
+    <form action="index.php?action=gboffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+
+  <!-- END MAIN -->
+  </div>
+ </div>
+</body>
+</html>
---
+++ views/diego.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/diego.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+<div class="nosybe">
+  <div class="main" id="main1">
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Le Grand Hôtel Diego est situé à Diego Suarez, à 800 mètres de Diego Suares. Il propose un casino, une piscine extérieure, un restaurant, un jardin et un salon commun.
+  </p>
+
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/GHD table 1.jpg" alt="Mountains" style="width:100%">
+        <h3>Le petit-déjeuner</h3>
+        <p>Des plats gourmants à découvrir <a href="photo/GHD table 1.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/GHD vue.jpg" alt="Lights" style="width:100%">
+        <h3>La vue</h3>
+        <p>Vous êtes la bienvenue<a href="photo/GHD vue.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/GHD chambre.jpg" alt="Nature" style="width:100%">
+        <h3>Chambre de luxe</h3>
+        <p>Ainsi que des salles de bains <a href="photo/GHD chambre.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/GHD piscine.jpg" alt="Mountains" style="width:100%">
+        <h3>La piscine</h3>
+        <p>Idéale pour se rafraîchir <a href="photo/GHD piscine.jpg">voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/logo GHD.jpg" alt="Bear" style="width:100%">
+    <h3>LE GRAND HOTEL DIEGO  <a href="photo/logo GHD.jpg">voir Logo</a></h3>
+    <form action="index.php?action=ravintsaraoffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+  <br>
+  <br>
+
+  <!-- END MAIN -->
+  </div>
+  <br>
+  <br>
+  <div class="main" id="main1">
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Situé à Diego Suarez, à quelques pas de la plage de Ramena, l'Hotel Lakana Ramena propose une piscine extérieure, un parking privé gratuit, un jardin et une plage privée. Situé à 42 km de la réserve de Fort d'Ambre et à 21 km de Port Antsiranana, il dispose d'un salon commun et d'une terrasse. Vous pourrez déguster des plats africains et indiens au restaurant ou prendre un cocktail au bar.
+  </p>
+
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/HLR vue.png" alt="Mountains" style="width:100%">
+        <h3>Une belle vue</h3>
+        <p>Propre envirennement autour <a href="photo/HLR vue.png">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/HLR table.png" alt="Lights" style="width:100%">
+        <h3>Resto-bar</h3>
+        <p>Des boissons et snacks de goûts spécials<a href="photo/HLR table.png">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/HLR chambre.png" alt="Nature" style="width:100%">
+        <h3>Chambre de luxe</h3>
+        <p>Des chambres haut de gamme <a href="photo/HLR chambre.png">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/HLR piscine.jpg" alt="Mountains" style="width:100%">
+        <h3>La piscine</h3>
+        <p>Idéale pour passer des moments innoubliables <a href="photo/HLR piscine.jpg">voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/logo HLR.jpg" alt="Bear" style="width:100%">
+    <h3>HOTEL LAKANA RAMENA  <a href="photo/logo HLR.jpg">voir Logo</a></h3>
+    <form action="index.php?action=exoraoffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+
+  <!-- END MAIN -->
+  </div>
+  <br>
+  <br>
+  <div class="main">
+    <br>
+    <br>
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Situé à Diego Suarez, à quelques pas de la plage de la baie de Sakalava, le Royal Sakalava propose un jardin, un parking privé gratuit, une plage privée et une terrasse. Il assure un service d'étage et possède une aire de jeux pour enfants. Vous pourrez déguster des plats chinois et français au restaurant ou prendre un cocktail au bar.
+  </p>
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/RS vue.png" alt="Mountains" style="width:100%">
+        <h3>La belle vue</h3>
+        <p>Des divers natures <a href="photo/RS vue.png">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/RS activité.jpg" alt="Lights" style="width:100%">
+        <h3>Relaxation</h3>
+        <p>Une petite réunion de famille <a href="photo/RS activité.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/RS chambre.png" alt="Nature" style="width:100%">
+        <h3>Vos chambres</h3>
+        <p>La simplicité  <a href="photo/RS chambre.png">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/RS table.jpg" alt="Mountains" style="width:100%">
+        <h3>Le petit-déjeuner</h3>
+        <p>Petits-déjeuner saines et équilibrés  <a href="">Voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/logo RS.jpg" alt="Bear" style="width:100%">
+    <h3>ROYAL SAKALAVA <a href="photo/logo RS.jpg">voir logo</a></h3>
+    <form action="index.php?action=gboffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+
+  <!-- END MAIN -->
+  </div>
+ </div>
+</body>
+</html>
