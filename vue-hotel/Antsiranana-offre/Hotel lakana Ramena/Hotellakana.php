b3b603870d103ce869b2d751d11871b8137b3104
Emeraude202 <andrianemeraude@gmail.com>
2024-11-12 14:18:24 +0300

    addoffre

---
+++ vue-hotel/Antsiranana-offre/Hotel lakana Ramena/Hotellakana.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/offreko.css">
+</head>
+<body>
+<div class="be">
+        <h2>Hotel Lakana Ramena</h2>
+        <div class="container">
+            <div class="box">
+                <div><img src="img/ramena5.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Escapade Romantique </p>
+                <div class="sebox"><p class="prix">85 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+            <div class="box">
+                <div><img src="img/ramena6.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Horizon Lointain </p>
+                <div class="sebox"><p class="prix">90 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+            <div class="box">
+                <div><img src="img/inconnu1.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Chambre du Bonheur </p>
+                <div class="sebox"><p class="prix">120 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ vue-hotel/Toamasina-offre/Streamliner/Streamliner.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/offreko.css">
+</head>
+<body>
+    <div class="be">
+        <h2>The Streamliner Hotel Apartement</h2>
+        <div class="container">
+            <div class="box">
+                <div><img src="img/streamliner4.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Suite ds Echos </p>
+                <div class="sebox"><p class="prix">125 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+            <div class="box">
+                <div><img src="img/streamliner5.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Chambre des Secrets </p>
+                <div class="sebox"><p class="prix">130 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+            <div class="box">
+                <div><img src="img/steamliner6.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Cocon d'Harmonie</p>
+                <div class="sebox"><p class="prix">135 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
