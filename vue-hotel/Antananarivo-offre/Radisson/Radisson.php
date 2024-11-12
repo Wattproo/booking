c36c41501653a4678a26139e8a578cf00e1ecde7
Emeraude202 <andrianemeraude@gmail.com>
2024-11-12 11:03:09 +0300

    push

---
+++ vue-hotel/Antananarivo-offre/Radisson/Radisson.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/offreko.css">
+</head>
+<body>
+    <div class="be">
+        <h2>Radisson Blu Hotel Waterfront</h2>
+        <div class="container">
+            <div class="box">
+                <div><img src="img/radisson4.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Chambre des Etoiles Filantes</p>
+                <div class="sebox"><p class="prix">200 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+            <div class="box">
+                <div><img src="img/radisson5.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Chambre du Paradis</p>
+                <div class="sebox"><p class="prix">180 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+            <div class="box">
+                <div><img src="img/radisson7.jpg" alt=""></div>
+                <ul>
+                    <li>Mada Travel.com</li>
+                </ul>
+                <p>Suite des Reveries</p>
+                <div class="sebox"><p class="prix">190 000 Ar</p></div>
+                <div class="button"><input type="button" id="mot" value="Reserver"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
