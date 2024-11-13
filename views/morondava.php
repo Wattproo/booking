7650f0c841f26c2fc78fbd7cccee27454644dc2e
Féru03 <72690924+jonathan295@users.noreply.github.com>
2024-11-13 12:16:19 +0300

    Il manque juste le border radius dans la page destination

---
+++ views/morondava.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/morondava.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+<div class="morondava">
+  <div class="main" id="main1">
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Situé à Morondava, à 400 mètres de la plage, l'Hôtel Baobab Café propose une piscine extérieure, un parking privé gratuit, un jardin et une terrasse. Il propose un restaurant, un service d'étage, une réception ouverte 24h/24 et une connexion Wi-Fi gratuite dans l'ensemble de ses locaux. Vous pourrez prendre un verre au bar.
+
+  </p>
+
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_45CCF972-CC93-4E8A-A60C-8DDCD4C534FE.jpeg" alt="Mountains" style="width:100%">
+        <h3>Le petit-déjeuner</h3>
+        <p>Des plats gourmants à découvrir <a href="photo/BR table.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_9A240F34-FB1F-4A6A-8F0F-AF8D66F7CE85.jpeg" alt="Lights" style="width:100%">
+        <h3>Chambres</h3>
+        <p>Spécialement conçu pour vous <a href="photo/BR vue.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_2C8FBF22-3007-478C-85B0-E2AD628FEACC.jpeg" alt="Nature" style="width:100%">
+        <h3>Restauration</h3>
+        <p>Bon appétit <a href="photo/BR chambre.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_A22168AD-AF85-4FD0-B063-F87733C48A45.jpeg" alt="Mountains" style="width:100%">
+        <h3>La piscine</h3>
+        <p>Idéale pour se rafraîchir <a href="photo/BR piscine.jpg">voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/Messenger_creation_C1EF28AF-C7F5-41AA-8AF0-127D7A014628.jpeg" alt="Bear" style="width:100%">
+    <h3>BAOBAB CAFE  <a href="photo/logo BR.jpg">voir Logo</a></h3>
+    <form action="index.php?action=ravintsaraoffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+  <br>
+  <br>
+
+  <!-- END MAIN -->
+  </div>
+  <br>
+  <br>
+  <div class="main" id="main1">
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Vous pouvez peut-être bénéficier d’une réduction Genius dans l’établissement LAGUNA BEACH. 
+  </p>
+
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_EE4BBBC2-D56A-4752-A175-0DCBB251ECEC.jpeg" alt="Mountains" style="width:100%">
+        <h3>Restaurant</h3>
+        <p>Boissons é snacks <a href="photo/MH table.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_7594A780-1085-4F56-B100-0CAA0461A546.jpeg" alt="Lights" style="width:100%">
+        <h3>Bungalows</h3>
+        <p>Vos appartements<a href="photo/MH vue.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_365DE0F6-27F0-4BEC-8672-B885D4C52166.jpeg" alt="Nature" style="width:100%">
+        <h3>Chambre de luxe</h3>
+        <p>Des chambres haut de gamme <a href="photo/MH chambre.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_A3DD6A7C-385B-4622-AD19-32DEB676C364.jpeg" alt="Mountains" style="width:100%">
+        <h3>La piscine</h3>
+        <p>Idéale pour passer des moments innoubliables <a href="photo/MH piscine.jpg">voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/Messenger_creation_003ADEDF-D3AF-46C9-A5B7-6E35FAD370A4.jpeg" alt="Bear" style="width:100%">
+    <h3>LAGUNA BEACH  <a href="photo/Messenger_creation_003ADEDF-D3AF-46C9-A5B7-6E35FAD370A4.jpeg">voir Logo</a></h3>
+    <form action="index.php?action=exoraoffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+
+  <!-- END MAIN -->
+  </div>
+  <br>
+  <br>
+  <div class="main">
+    <br>
+    <br>
+
+  <h1 style="color:white;">MADA_TRAVEL.COM</h1>
+  <hr>
+  <p>
+  Vous pouvez peut-être bénéficier d’une réduction Genius dans l’établissement CAP KIMONY. Les réductions Genius proposées par cet établissement dépendent des dates de réservation, des dates de séjour et des autres offres disponibles.
+  </p>
+  <!-- Portfolio Gallery Grid -->
+  <div class="row">
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_775CE9C8-1289-4EA8-8630-DBC5B6A5DD83.jpeg" alt="Mountains" style="width:100%">
+        <h3>La belle vue</h3>
+        <p>Au bord de la mer <a href="photo/SH table.png">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_D58FED51-4180-41D1-84B4-150390276F60.jpeg" alt="Lights" style="width:100%">
+        <h3>Relaxation</h3>
+        <p>Pour se détendre <a href="photo/SH activité.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_5D073590-552B-44D8-BC89-69AA900F8C5A.jpeg" alt="Nature" style="width:100%">
+        <h3>Vos chambres</h3>
+        <p>A la traditionnelle  <a href="photo/SH chambre.jpg">voir</a></p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <img src="photo/Messenger_creation_5428A049-6306-44C3-BD3A-455C0E6EA7EF.jpeg" alt="Mountains" style="width:100%">
+        <h3>Déjeuner</h3>
+        <p>Profitez de vos moments de qualités <a href="photo/SH table.png">Voir</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <img src="photo/lastImage.jpeg" alt="Bear" style="width:100%">
+    <h3>CAP KIMONY <a href="photo/lastImage.jpeg">voir logo</a></h3>
+    <form action="index.php?action=gboffre" method="post">
+      <input type="submit" value="voir les offres" class="btn btn-primary">
+  </form>
+  </div>
+
+  <!-- END MAIN -->
+  </div>
+ </div>
+
+</body>
+</html>
